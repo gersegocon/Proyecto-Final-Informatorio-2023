19bb675c5f562be48af3a731ba3f645bff2353dc
gersegocon <german.segoviacon@gmail.com>
2023-12-02 01:52:32 -0300

    Filtros agregados y desplegables

--- blog/app/noticias/views.py
+++ blog/app/noticias/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
-from .models import Noticia
+from .models import Noticia, Categoria
 # Create your views here.
 def ListarNoticias(request):
     contexto = {}
+    id_categoria = request.GET.get("id", None)
+
+    if id_categoria:
+        n = Noticia.objects.filter(categoria_noticia=id_categoria).order_by('-fecha_publicacion')
+    else:
+        n = Noticia.objects.all().order_by('-fecha_publicacion')
+    
+    antiguedad_asc = request.GET.get("antiguedad_asc")
+    if antiguedad_asc:
+        n = Noticia.objects.all().order_by('fecha_publicacion')
+
+    antiguedad_des = request.GET.get("antiguedad_des")
+    if antiguedad_des:
+        n = Noticia.objects.all().order_by('-fecha_publicacion')
 
     
-    n = Noticia.objects.all().order_by('-fecha_publicacion')
-    
+    orden_asc = request.GET.get("orden_asc")
+    if orden_asc:
+        n = Noticia.objects.all().order_by('titulo')
+
+    orden_des = request.GET.get("orden_des")
+    if orden_des:
+        n = Noticia.objects.all().order_by('-titulo')
+
+    cat = Categoria.objects.all().order_by('nombre')
+
     contexto['noticias'] = n
+    contexto['categorias'] = cat
 
     return render(request, 'noticias/listar.html', contexto)
 
