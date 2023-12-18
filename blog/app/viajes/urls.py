f815b629b489018b0ab1acd9e3178a242554dd00
luciano <luciano-mihoff@hotmail.com>
2023-12-18 12:42:59 -0300

    se agrega url de edición

--- blog/app/viajes/urls.py
+++ blog/app/viajes/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
      path('', views.ListarViajes, name='listar'),
      path('detalle/<int:pk>', views.DetalleViajes, name='detalle'),
      path('addViaje', views.AddViaje, name='addViaje'),
+     path('viajes/<int:pk>/editar', views.EditarViajes, name='edit_viaje'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
